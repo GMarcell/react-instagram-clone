a6f80e0205ef5b5bdb4df61c43aa173c9cfc1194
GMarcell <grand1310marcell@gmail.com>
2022-03-15 15:32:02 +0700

    Profile Header Done

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,4 +1,6 @@
+import { AddBoxOutlined, Inbox, Menu } from '@mui/icons-material'
 import React, { useEffect, useState } from 'react'
+import './css/HeaderNav.css'
 
 function HeaderNav(props) {
     const [show, handleShow] = useState(false)
@@ -14,7 +16,11 @@ function HeaderNav(props) {
     }, [])
     return (
         <div className={`headerNav ${show && "header__black"}`}>
-            <h1>{props.username}</h1>
+            <h2 className='headerNav__username'>{props.username}</h2>
+            <div className='headerNav__btn'>
+                <AddBoxOutlined fontSize='large'/>
+                <Menu fontSize='large' className='headerNav__menu'/>
+            </div>
         </div>
     )
 }
